4e73c79f4c432d0fb445f50b2499e2bdb712ab48
Harper <u6503561@anu.edu.au>
2019-05-05 19:24:13 +1000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // set the activity when press the button
+    public void buttonPress(View v){
+        EditText et = findViewById(R.id.editText);
+        // clear text
+        et.setText("");
+    }
+}
